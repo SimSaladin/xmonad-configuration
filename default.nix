4b478cb19ab8d9c997c00e1b3e404a847bc0c0f9
Samuli Thomasson <samuli.thomasson@pm.me>
2021-12-12 00:14:06 +0100

    fix alsa-plugins

--- default.nix
+++ default.nix
@@ -10,6 +10,8 @@
 let
   pkgs = import nixpkgsSrc nixpkgsArgs;
   llvm = pkgs.${llvmVersion};
+  alsa-plugins = pkgs.alsa-plugins.override { libjack2 = false; };
+
   project = pkgs.haskell-nix.stackProject' {
     src = pkgs.haskell-nix.haskellLib.cleanGit {
       name = "xmonad-configuration";
@@ -21,12 +23,20 @@ let
         # Optimize and use LLVM
         packages.xmonad-configuration.flags.optimize = optimize;
         packages.xmonad-configuration.flags.via-llvm = useLLVM;
-        packages.xmonad-configuration.components.exes.xmonad-x86_64-linux.libs = lib.optionals useLLVM [ llvm ];
+        packages.xmonad-configuration.components.exes.xmonad-x86_64-linux = {
+          libs = lib.optionals useLLVM [ llvm ];
+          # Fix alsa-plugins. There's a patch in nixpkgs#alsa-lib that adds support for the ALSA_PLUGIN_DIR variable.
+          # We need to set it at runtime. Also, note that the alsa-plugins-wrapper script uses the wrong
+          # variable name (it's broken).
+          #
+          # https://github.com/NixOS/nixpkgs/issues/6860
+          build-tools = [ pkgs.makeWrapper ];
+          postInstall = "wrapProgram $out/bin/xmonad-x86_64-linux --set ALSA_PLUGIN_DIR ${alsa-plugins}/lib/alsa-lib";
+        };
         # xmobar requires some libraries that reside in the xorg attrset
         packages.xmobar.components.library.libs = lib.mkForce (with pkgs.xorg; [ libXrandr libXrender libXpm ]);
       })
     ];
-    #materialized = ./.materialized;
   };
 in
 project // {
